2ff9032758df325749bcee60cd3779df2bdda0d8
sayed24 <sayedelabd04@gmail.com>
2018-02-09 17:43:28 +0200

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,6 @@ http.createServer(function (request, response) {
 
    // Send the response body as "Hello World"
    response.end('Hello World\n');
-}).listen(3000);
+}).listen(8085);
 
-// Console will print the message
-console.log('Server running at http://127.0.0.1:3000/');
+
